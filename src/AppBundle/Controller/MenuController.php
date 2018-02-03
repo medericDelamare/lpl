153dc4c25412dd8452edc5763e58d66527fa1d01
mdelamare <mederic.delamare@trsb.net>
2018-02-03 01:23:32 +0100

    feat(menus): renvoi des menus + style

--- src/AppBundle/Controller/MenuController.php
+++ src/AppBundle/Controller/MenuController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Categorie;
+use AppBundle\Entity\Menu;
 use AppBundle\Entity\Plat;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -17,9 +18,10 @@ class MenuController extends Controller
     {
         /** @var Categorie[] $categories */
         $categories = $this->getDoctrine()->getRepository(Categorie::class)->findAll();
-
+        $menus = $this->getDoctrine()->getRepository(Menu::class)->findAll();
         return $this->render('menu.html.twig', [
-            'categories' => $categories
+            'categories' => $categories,
+            'menus' => $menus
         ]);
     }
 }
